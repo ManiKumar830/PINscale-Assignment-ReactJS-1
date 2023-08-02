aea2609bb165504fd723c66c475d6e8afda38f35
ManiKumar830 <mohanmanikumar8309@gmail.com>
2023-08-02 16:36:44 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import {Route, Switch, Redirect} from 'react-router-dom'
+
+import LoginForm from './components/LoginForm'
+import NotFound from './components/NotFound'
+import AdminDashboard from './components/AdminPage/AdminDashBoard'
+import AdminAllTransactions from './components/AdminPage/AdminAllTransactions'
+
+import UserDashBoard from './components/UserPage/UserDashBoard'
+import UserAllTransactions from './components/UserPage/UserAllTransactions'
+
+import UserProfilePage from './components/UserPage/UserProfilePage'
+import AdminProfilePage from './components/AdminPage/AdminProfilePage'
+import ProtectedRoute from './components/ProtectedRoute'
+
+import './App.css'
+
+const App = () => (
+  <>
+    <Switch>
+      <Route exact path="/" component={LoginForm} />
+      <ProtectedRoute
+        exact
+        path="/admin/dashboard"
+        component={AdminDashboard}
+      />
+      <ProtectedRoute
+        exact
+        path="/admin/transactions"
+        component={AdminAllTransactions}
+      />
+
+      <ProtectedRoute
+        exact
+        path="/admin/profile"
+        component={AdminProfilePage}
+      />
+
+      <ProtectedRoute exact path="/user/dashboard" component={UserDashBoard} />
+      <ProtectedRoute
+        exact
+        path="/user/transactions"
+        component={UserAllTransactions}
+      />
+      <ProtectedRoute exact path="/user/profile" component={UserProfilePage} />
+      <Route path="/not-found" component={NotFound} />
+      <Redirect to="not-found" />
+    </Switch>
+  </>
+)
+
+export default App
